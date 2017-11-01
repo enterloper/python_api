46f023e192393f54dffafe43f9c886091143e121
Rich Boothe <richardjboothe@gmail.com>
2017-10-31 22:39:21 -0500

    create users resources

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
 from flask import Flask
 
+import config
 import models
 from resources.courses import courses_api
 from resources.reviews import reviews_api
-
-DEBUG = True
-HOST = '0.0.0.0'
-PORT = 8000
-
+from resources.users import users_api
 app = Flask(__name__)
 app.register_blueprint(courses_api)
 app.register_blueprint(reviews_api, url_prefix='/api/v1')
+app.register_blueprint(users_api, url_prefix='/api/v1')
+
 
 @app.route('/')
 def hello_world():
@@ -19,4 +18,4 @@ def hello_world():
 
 if __name__ == '__main__':
 	models.initialize()
-	app.run(debug=DEBUG, host=HOST, port=PORT)
+	app.run(debug=config.DEBUG, host=config.HOST, port=config.PORT)
--- models.py
+++ models.py
@@ -1,8 +1,43 @@
 import datetime
 
+from argon2 import PasswordHasher
 from peewee import *
 
+import config
+
 DATABASE = SqliteDatabase('courses.sqlite')
+HASHER = PasswordHasher()
+
+
+class User(Model):
+	username = CharField(unique=True)
+	email = CharField(unique=True)
+	password = CharField()
+
+	class Meta:
+		database = DATABASE
+
+	@staticmethod
+	def set_password(password):
+		return HASHER.hash(password)
+
+	def verify_password(self, password):
+		return HASHER.verify(self.password, password)
+
+	@classmethod
+	def create_user(cls, username, email, password, **kwargs):
+		email = email.lower()
+		try:
+			cls.select().where(
+				(cls.email == email)|(cls.username**username)
+			).get()
+		except cls.DoesNotExist:
+			user = cls(username=username, email=email)
+			user.password = user.set_password(password)
+			user.save()
+			return user
+		else:
+			raise Exception('User with that email/username already exists')
 
 
 class Course(Model):
@@ -19,12 +54,12 @@ class Review(Model):
 	rating = IntegerField()
 	comment = TextField(default='')
 	created_at = DateTimeField(default=datetime.datetime.now)
-
+	created_by = ForeignKeyField(User, related_name='review_set')
 	class Meta:
 		database = DATABASE
 
 
 def initialize():
 	DATABASE.connect()
-	DATABASE.create_tables([Course, Review], safe=True)
+	DATABASE.create_tables([User, Course, Review], safe=True)
 	DATABASE.close()
